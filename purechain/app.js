454821231f61785671ff30676f3da235bdd94262
GayanthaSandeep16 <gayantha.20210118@iit.ac.lk>
2025-01-25 00:23:00 +0530

    fileupload changes

--- purechain/app.js
+++ purechain/app.js
@@ -1,18 +1,25 @@
 require('dotenv').config();
 const express = require('express');
-const dataRoutes = require('./routes/data');
+const fileUpload = require("express-fileupload");
 
 const app = express();
-app.use(express.json());
+const dataRoutes = require('./routes/data');
 const port = process.env.PORT || 3000;
+
+
+app.use(express.json());
+app.use(fileUpload());
+
+
 
 
 // Middleware to handle routes
 app.use('/api',dataRoutes );
 
+
 // Start server
 const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
